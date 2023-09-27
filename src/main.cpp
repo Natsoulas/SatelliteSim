734d248d8a888cf93965a162295a5abbc6698667
Nicholas Natsoulas <nrn22@cornell.edu>
2023-09-27 02:20:59 -0400

    first cmake build + printed out an eigen vector :)

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,13 @@
 #include <fstream>
 #include <string>
 #include <Eigen/Dense>
+using namespace std;
+using namespace Eigen;
 
 int main() {
-    std::cout << "Hello World";
+    // Proof that I can use Eigen in C++ Dev :)
+    Vector3d vec(3);
+    vec << 6.9, 4.2, 0.2;
+    cout << "Hello Eigen: " << vec << endl;
+    return 0;
 }
